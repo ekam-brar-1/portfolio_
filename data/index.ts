b45e09864e2b60f069c929570cd6a42cba98619b
Ekamjot Singh <160436305+ekam-brar-1@users.noreply.github.com>
2025-04-04 18:39:39 -0600

    ostly done

---
+++ data/index.ts
@@ -0,0 +1,252 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Resume", link: "https://drive.google.com/file/d/1AHGfnpD__geEidlzs2nfswTxkXW0yt3K/view" },
+    { name: "Projects", link: "#projects" },
+
+    
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize open communication with my clients for the best quality results",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: " Flexibility is my promise ",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2 min-h-[40vh] select-none",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My Resume",
+      description: "Are you hiring? Check out ",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2 flex",
+      imgClassName: "",
+      titleClassName: "justify-end",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Clean Code and Modern Design",
+      description: "Passionate about transforming ideas into reality.",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+ export const projects = [
+  {
+    id: 1,
+    title: "Rolla",
+    des: "Rolla is a customizable shopping website that allows users to personalize their online shopping experience with dynamic themes and product configurations.",
+    img: "/rolla.png", 
+    iconLists: ["/react.png", "/tailwind.png", "/nodejs.png", "/mongodb.png", "/aws.png"],
+    link: "https://github.com/ekam-brar-1/metaverse-frontend", 
+  },
+  {
+    id: 2,
+    title: "2D Metaverse-Chat",
+    des: "2D Metaverse-Chat is an innovative chat platform set in a 2D virtual environment, enabling immersive social interactions and community engagement.",
+    img: "/meta.png", 
+    iconLists: ["/react.png", "/socket.png", "/tailwind.png", "/nodejs.png"],
+    link: "https://github.com/ekam-brar-1", 
+  },
+  {
+    id: 3,
+    title: "AI Expense Tracker",
+    des: "The AI Expense Tracker leverages artificial intelligence to categorize and analyze expenses, providing insights and predictive budgeting to simplify financial management.",
+    img: "/spendsay.png", // Replace with your actual image path
+    iconLists: ["/expo.png", "/ai.png", "/supa.jpeg"],
+    link: "https://github.com/ekam-brar-1/expense-tracker", // Update with your project URL
+  },
+  {
+    id: 4,
+    title: "Hands On Touring",
+    des: "Hands On Touring is a dynamic travel and tour booking platform that offers interactive itineraries, real-time updates, and a seamless booking experience for travelers.",
+    img: "/hands.png", // Replace with your actual image path
+    iconLists: ["/react.png", "/nodejs.png", "/mongodb.png", "/supa.jpeg"],
+    link: "https://github.com/ekam-brar-1/expense-tracker", // Update with your project URL
+  },
+];
+
+  export const testimonials = [
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+ 
+  export const workExperience = [
+  {
+    id: 1,
+    title: "MongoDB Node.js Developer Path",
+    issuer: "MongoDB",
+    issued: "Feb 2025",
+    credentialId: "MDBd0ygy30fb1",
+    skills: ["Database Management System (DBMS)"],
+    thumbnail: "/mongodb.png", // update with your image path
+    credentialUrl: "https://learn.mongodb.com/c/3KBSrml3RE2DtghhGlGqUQ", // link to the credential details if available
+  },
+  {
+    id: 2,
+    title: "Using MongoDB with Node.js",
+    issuer: "MongoDB",
+    issued: "Feb 2025",
+    credentialId: "MDB4wm2zx0lgs",
+    skills: ["Database Management System (DBMS)"],
+    thumbnail: "/mongodb.png", // update with your image path
+    credentialUrl: "https://learn.mongodb.com/c/O7ovfiwwTO-fKpgjBK-8ag",
+  },
+  {
+    id: 3,
+    title: "JavaScript Essential Training",
+    issuer: "LinkedIn",
+    issued: "Jan 2025",
+    credentialId: "", // add Credential ID if available
+    skills: ["JavaScript"],
+    thumbnail: "/linkedIn.png", // update with your image path
+    credentialUrl: "https://www.linkedin.com/learning/certificates/9c749fa693f7ffdd277d01d6d0cd200756acc6e0912a30521f51c8dc5458942c?u=2245281",
+  },
+  {
+    id: 4,
+    title: "Career Essentials in GitHub Professional Certificate",
+    issuer: "GitHub",
+    issued: "Aug 2024",
+    credentialId: "", // add Credential ID if available
+    skills: ["GitHub"],
+    thumbnail: "/git.svg", // update with your image path
+    credentialUrl: "https://www.linkedin.com/learning/certificates/2c66649ae43b5d81e31a94c8528b105bbb519abfaa4ab125a6e77acf6d46db98?u=2245281",
+  },
+  {
+    id: 5,
+    title: "Career Essentials in Software Development by Microsoft and LinkedIn",
+    issuer: "Microsoft",
+    issued: "Aug 2024",
+    credentialId: "", // add Credential ID if available
+    skills: ["Software Development", "Programming"],
+    thumbnail: "/microsoft.png", // update with your image path
+    credentialUrl: "https://www.linkedin.com/learning/certificates/55a457cab5c83a1d02cc04ad27256ec7f844f8cd7f372dc261180acc49745293?u=2245281",
+  },
+  {
+    id: 6,
+    title: "Docker Foundations Professional Certificate",
+    issuer: "Docker, Inc",
+    issued: "Aug 2024",
+    credentialId: "", 
+    skills: ["Containerization", "Docker Products"],
+    thumbnail: "/dock.svg",
+    credentialUrl: "https://www.linkedin.com/learning/certificates/b6dfc23931de582eec204c5297e53b83d2a289d448a7e94bf0b6a50a862ccc76?u=2245281",
+  },
+];
+
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      href: "https://github.com/ekam-brar-1",
+    },
+   
+    {
+      id: 3,
+      img: "/link.svg",
+      href: "https://www.linkedin.com/in/ekamjot-singh123/",
+    },
+  ];
